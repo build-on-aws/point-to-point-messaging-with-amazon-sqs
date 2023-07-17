75e95afc99c8b37ea157101fbc09ea063412f11a
Dennis Traub <dennis.traub@gmail.com>
2023-07-17 14:28:29 +0200

    Complete sample code after step 2 (#2)
    * Create empty CDK project
    * Add an SQS queue
    * Add README
    
    ---------
    
    Co-authored-by: Dennis Traub <traubd@amazon.de>

---
+++ point-to-point-example/lib/point-to-point-example-stack.ts
@@ -0,0 +1,13 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class PointToPointExampleStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const queue = new sqs.Queue(this, 'Queue', {
+      queueName: 'MyQueue',
+    });
+  }
+}
